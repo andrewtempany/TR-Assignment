6dc4ff4f02b21744367b3fa8e4efdb0b8e8e63f9
andrewtempany <andrewtempany95@gmail.com>
2020-10-13 08:53:05 +1300

    slice to avoid first line

--- andysquestion.py
+++ andysquestion.py
@@ -2,24 +2,36 @@
 
 import sys
 
-filepath = "Desktop/testfile.txt"
+filepath = "testFile.csv"
 
 inFile = open(filepath, "r")
+lines = inFile.readlines()
 compdict = {}
 
-for line in inFile:
-	elems = line.split(",")
-	if elems[0] not in compdict:
-		compdict[elems[0]] = 0
-		compdict[elems[0]] += float(elems[1])
-		compdict[elems[0]] += float(elems[2])*-1
-	else:
-		compdict[elems[0]] += float(elems[1])
-		compdict[elems[0]] += float(elems[2])*-1
+for line in lines[1:]:
 
-compdict['calvin'] += compdict['cal']
-compdict['calvin'] += compdict['charlie']
-del compdict['cal']
-del compdict['charlie']
+    elems = line.split(",")
+
+    if elems[0] not in compdict:
+        compdict[elems[0]] = 0
+        compdict[elems[0]] += float(elems[1])
+        compdict[elems[0]] += float(elems[2])*-1
+    else:
+        compdict[elems[0]] += float(elems[1])
+        compdict[elems[0]] += float(elems[2])*-1
+
+compdict['Utes'] += compdict['Utes LLC']
+compdict['Utes'] += compdict['LLC Utes']
+compdict['Utes'] += compdict['Utes Limited Liability Corp']
+compdict['Utes'] += compdict['Utes Co.']
+del compdict['LLC Utes']
+del compdict['Utes Limited Liability Corp']
+del compdict['Utes Co.']
+del compdict['Utes LLC']
+
+
+# utesNames = ["Utes", "Utes LLC", "LLC Utes",
+#              "Utes Limited Liability Corp", "Utes Co."]
+
 
 print(compdict)
